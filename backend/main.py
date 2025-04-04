4792c65ed3df1499a3acb72a9cc0fda92c01328c
Aleksandra Tulovska <aleksandratulovska@Aleksandras-MacBook-Air.local>
2025-04-04 13:59:33 +0300

    Main.py FastAPI and CORS setup

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# Create the FastAPI app
+app = FastAPI()
+
+# Setting up CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # frontend server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# test
+@app.get("/api/hello")
+def read_root():
+    return {"message": "Hello from FastAPI!"}
