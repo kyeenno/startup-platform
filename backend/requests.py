a51233f5ad3588c5b0f104c2aab22e1c55b252cb
een <121685444+kyeenno@users.noreply.github.com>
2025-04-07 11:24:16 +0300

    Changed variable name

--- backend/requests.py
+++ backend/requests.py
@@ -8,20 +8,20 @@ from google.analytics.data_v1beta.types import RunReportRequest, DateRange, Metr
 
 load_dotenv()
 
-client_id = os.environ.get("CLIENT_ID")
-client_secret = os.environ.get("CLIENT_SECRET")
+client_id = os.environ.get("GOOGLE_CLIENT_ID")
+client_secret = os.environ.get("GOOGLE_CLIENT_SECRET")
 
 def create_creds():
     flow = InstalledAppFlow.from_client_config(
         {
             "installed": {
-            "client_id": client_id,
-            "project_id": "southern-sol-455810-d7",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_secret": client_secret,
-            "redirect_uris": ["http://localhost"]
+                "client_id": client_id,
+                "project_id": "southern-sol-455810-d7",
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret": client_secret,
+                "redirect_uris": ["http://localhost"]
             }
         },
         scopes=["https://www.googleapis.com/auth/analytics.readonly"]
