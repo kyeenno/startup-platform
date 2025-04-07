2ac5e2a3105e617fea2e6eadb02cf3e7b51b734b
een <121685444+kyeenno@users.noreply.github.com>
2025-04-07 11:22:08 +0300

    Set up Google Analytics

---
+++ backend/requests.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.analytics.data_v1beta import BetaAnalyticsDataClient
+from google.analytics.data_v1beta.types import RunReportRequest, DateRange, Metric, Dimension
+
+
+load_dotenv()
+
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+def create_creds():
+    flow = InstalledAppFlow.from_client_config(
+        {
+            "installed": {
+            "client_id": client_id,
+            "project_id": "southern-sol-455810-d7",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_secret": client_secret,
+            "redirect_uris": ["http://localhost"]
+            }
+        },
+        scopes=["https://www.googleapis.com/auth/analytics.readonly"]
+    )
+
+    credentials = flow.run_local_server(port=8000)
+    return BetaAnalyticsDataClient(credentials=credentials)
