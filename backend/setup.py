d1f953c922344b30c732964b76cdd9b257889df0
een <121685444+kyeenno@users.noreply.github.com>
2025-04-07 16:56:03 +0300

    Added token

--- backend/setup.py
+++ backend/setup.py
@@ -12,27 +12,46 @@ load_dotenv()
 client_id = os.environ.get("GOOGLE_CLIENT_ID")
 client_secret = os.environ.get("GOOGLE_CLIENT_SECRET")
 property_id = os.environ.get("GOOGLE_PROPERTY_ID")
+
+# Token file path
 token_file = "token.json"
+
+SCOPES = ["https://www.googleapis.com/auth/analytics.readonly"]
 
 # Defining client creentials
 def create_creds():
-    flow = InstalledAppFlow.from_client_config(
-        {
-            "installed": {
-                "client_id": client_id,
-                "project_id": "southern-sol-455810-d7",
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_secret": client_secret,
-                "redirect_uris": ["http://localhost/"]
-            }
-        },
-        scopes=["https://www.googleapis.com/auth/analytics.readonly"]
-    )
+    creds = None
 
-    credentials = flow.run_local_server(port=8000)
-    return BetaAnalyticsDataClient(credentials=credentials)
+    # Load from file if exists
+    if os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+
+    # If no valid creds, do the OAuth flow
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh()  # auto-refresh if token expired
+        else:
+            flow = InstalledAppFlow.from_client_config(
+                {
+                    "installed": {
+                        "client_id": client_id,
+                        "project_id": "southern-sol-455810-d7",
+                        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                        "token_uri": "https://oauth2.googleapis.com/token",
+                        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                        "client_secret": client_secret,
+                        "redirect_uris": ["http://localhost/"]
+                    }
+                },
+                scopes=SCOPES
+            )
+            creds = flow.run_local_server(port=8000)
+
+    # Save token for next time
+    with open(token_file, "w") as token:
+            token.write(creds.to_json())
+
+    return BetaAnalyticsDataClient(credentials=creds)
 
 client = create_creds()
 
