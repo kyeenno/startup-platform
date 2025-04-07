c882c217014b7f5bfa011b0447f5c7d3be58b605
een <121685444+kyeenno@users.noreply.github.com>
2025-04-07 12:08:16 +0300

    Setup Google analytics auth

--- backend/requests.py
+++ backend/requests.py
@@ -5,14 +5,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunReportRequest, DateRange, Metric, Dimension
 
-
 load_dotenv()
 
-# Getting env variables
 client_id = os.environ.get("GOOGLE_CLIENT_ID")
 client_secret = os.environ.get("GOOGLE_CLIENT_SECRET")
+property_id = os.environ.get("GOOGLE_PROPERTY_ID")
 
-# Defining required credentials
 def create_creds():
     flow = InstalledAppFlow.from_client_config(
         {
@@ -23,7 +21,7 @@ def create_creds():
                 "token_uri": "https://oauth2.googleapis.com/token",
                 "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
                 "client_secret": client_secret,
-                "redirect_uris": ["http://localhost"]
+                "redirect_uris": ["http://localhost/"]
             }
         },
         scopes=["https://www.googleapis.com/auth/analytics.readonly"]
@@ -31,3 +29,22 @@ def create_creds():
 
     credentials = flow.run_local_server(port=8000)
     return BetaAnalyticsDataClient(credentials=credentials)
+
+client = create_creds()
+
+#query
+request = RunReportRequest(
+    property=f"properties/{property_id}",
+    dimensions=[Dimension(name="date")],
+    metrics=[Metric(name="sessions"), Metric(name="totalUsers")],
+    date_ranges=[DateRange(start_date="2024-01-01", end_date="2024-01-04")],
+)
+
+response = client.run_report(request)
+
+#print
+for row in response.rows:
+    date = row.dimension_values[0].value
+    sessions = row.metric_values[0].value
+    users = row.metric_values[1].value
+    print(f"{date}: Sessions = {sessions}, Users = {users}")
