140ae37b12974d0c65101853c885a4c9eeb81fcc
Aleksandra Tulovska <aleksandratulovska@Aleksandras-MacBook-Air.local>
2025-04-14 16:51:21 +0300

    Logged in user data fetching in Main

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,45 @@
-from fastapi import FastAPI
+from fastapi import Request, HTTPException, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from auth import verify_token # function from auth.py
 
 # Create the FastAPI app
 app = FastAPI()
 
-# Setting up CORS
+# Allow frontend/mobile access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], # frontend server
+    allow_origins=["*"], # frontend server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/api/summary")
+async def get_summary(request: Request): # function that handles request
+    auth_header = request.headers.get("authorization") # extracting Authorization header
+    if not auth_header or not auth_header.startswith("Dearer "):
+        raise HTTPException(status_code=401, detail="Missing or invalid token")
+    token = auth_header.split(" ")[1] # Bearer <token> <- to take just token part
+
+    try:
+        payload = verify_token(token) # verifying token using function from auth.py
+        user_id = payload.get("sub") # Supabase UID
+        email = payload.get("email")
+        
+        # Test print
+        print("Authenticated user:", user_id, email)
+
+        # Return to frontend
+        return {
+            "sessions": 123,
+            "total_users": 456,
+            "user_id": user_id,
+            "email": email
+        }
+    
+    except Exception as e: # if token is invalid raise error
+        raise HTTPException(status_code=403, detail=f"Invalid token: {e}")
+
 
 # test
 @app.get("/")
