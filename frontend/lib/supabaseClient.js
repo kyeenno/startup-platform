501138c2c4f61e29a38a6b2b64e1dfa1e231c8c8
een <121685444+kyeenno@users.noreply.github.com>
2025-04-16 01:58:38 +0300

    Reconfigured supabase client

--- frontend/lib/supabaseClient.js
+++ frontend/lib/supabaseClient.js
@@ -5,5 +5,13 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-// Creates client (gives access to authorisation)
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Creates client (gives access to authorisation) + configure sessions and auto refresh
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: false,
+        storageKey: 'supabase-auth',
+        storage: typeof window !== 'undefined' ? window.localStorage : undefined
+    }
+});
